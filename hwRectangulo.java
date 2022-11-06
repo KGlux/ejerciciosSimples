bf2fc853c2e117f3426c0785b4a96afe59ec65fd
BadKat <109204463+KGlux@users.noreply.github.com>
2022-11-05 20:30:21 -0600

    Update hwRectangulo.java

--- hwRectangulo.java
+++ hwRectangulo.java
@@ -6,22 +6,6 @@ public class hwRectangulo {
 
     public static void main(String[] args) {
 
-        /*En el siguiente ejercicio se solicita calcular el área y el perímetro de un Rectángulo, para ello deberemos
-        crear las siguientes variables:
-        alto (int)
-        ancho (int)
-        El usuario debe proporcionar los valores de largo y ancho, y después se debe imprimir el resultado en el siguiente
-        formato(no usar acentos y respetar los espacios, mayúsculas, minúsculas y saltos de línea):
-        #Proporciona el alto:
-        #Proporciona el ancho:
-        Area: <area>
-        Perimetro: <perimetro>
-        #Las fórmulas para calcular el área y el perímetro de un Rectángulo son:
-        #Área: alto * ancho
-        #Perímetro: (alto + ancho) * 2
-        * */
-
-        /* +++++++++++ RESOLUCION DEL PROBLEMA +++++++++++ */
         int altura;
         int ancho;
         float resultArea;
