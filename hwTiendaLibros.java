681591bdc8321ad0cc917fade57b27c7720e1d08
BadKat <109204463+KGlux@users.noreply.github.com>
2022-11-05 20:27:51 -0600

    Add files via upload

---
+++ hwTiendaLibros.java
@@ -0,0 +1,30 @@
+package ejerciciosUdemy;
+
+import java.util.Scanner;
+
+public class hwTiendaLibros {
+
+    public static void main(String[] args) {
+
+        /* Hacer ejercicio leccion 24 */
+
+        String nombre;
+        int id;
+        double price;
+        boolean envioFree;
+
+        Scanner dato = new Scanner(System.in);
+        System.out.print("Ingrese su nombre: ");
+        nombre = dato.nextLine();
+        System.out.print("Ingrese su ID: ");
+        id = dato.nextInt();
+        System.out.print("Precio: ");
+        price = dato.nextDouble();
+        System.out.print("Es gratis el envio: ");
+        envioFree = dato.nextBoolean();
+
+        System.out.println(nombre + " ID: " + id);
+        System.out.println("$: "+ price +" Envio Gartuito: "+envioFree);
+
+    }
+}
