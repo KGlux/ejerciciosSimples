cbad4a8c35457adaf91af86da4c22e01b9b15724
BadKat <109204463+KGlux@users.noreply.github.com>
2022-11-05 20:30:59 -0600

    Update hwElMayorDeDosNumeros.java

--- hwElMayorDeDosNumeros.java
+++ hwElMayorDeDosNumeros.java
@@ -6,19 +6,6 @@ public class hwElMayorDeDosNumeros {
 
     public static void main(String[] args) {
 
-        /*Solicitar al usuario dos valores:
-        numero1 (int)
-        numero2 (int)
-        Se debe imprimir el mayor de los dos números (la salida debe ser identica a la que sigue):
-        Proporciona el numero1:
-        Proporciona el numero2:
-        El numero mayor es:
-        <numeroMayor>
-        Puedes utilizar el IDE de tu preferencia para codificar la solución y después pegar tu solución en esta herramienta.
-
-        Preguntas de esta tarea
-        ¿Cuál es el código del requerimiento solicitado?
-        */
         int num1;
         int num2;
 
