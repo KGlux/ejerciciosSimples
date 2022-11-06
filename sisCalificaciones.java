0f12be126427dfea8c1d01bec6aeff2bc6ec9085
BadKat <109204463+KGlux@users.noreply.github.com>
2022-11-05 20:29:49 -0600

    Update sisCalificaciones.java

--- sisCalificaciones.java
+++ sisCalificaciones.java
@@ -6,19 +6,6 @@ public class sisCalificaciones {
 
     public static void main(String[] args) {
 
-        /*El objetivo del ejercicio es crear un sistema de calificaciones, como sigue:
-            El usuario proporcionará un valor entre 0 y 10.
-            Si está entre 9 y 10: imprimir una A
-            Si está entre 8 y menor a 9: imprimir una B
-            Si está entre 7 y menor a 8: imprimir una C
-            Si está entre 6 y menor a 7: imprimir una D
-            Si está entre 0 y menor a 6: imprimir una F
-            cualquier otro valor debe imprimir: Valor desconocido
-            Por ejemplo:
-            Proporciona un valor entre 0 y 10: A
-        */
-
-        /* +++++++++++ RESOLUCION DEL PROBLEMA +++++++++++ */
         int calificacion;
 
         calificacion = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese la calificacion"));
